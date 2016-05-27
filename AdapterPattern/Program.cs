e59cc4f896489034deb0fa8b97e16f909b642d91
Neil Pullinger <ncpullinger+github@gmail.com>
2016-05-27 21:16:27 +0100

    Added adapter pattern project

---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var turkey = new WildTurkey();
+            var turkeyAdapter = new TurkeyAdapter(turkey);
+
+            turkeyAdapter.Fly();
+            turkeyAdapter.Quack();
+        }
+    }
+}
